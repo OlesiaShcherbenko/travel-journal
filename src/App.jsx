b2d9271eff6d08ee5238e5a0439ad9b5cbc2e0e8
Olesia Shcherbenko <oshch96@gmail.com>
2025-01-11 19:29:17 +0100

    feature/pass-props-to-entry-component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,44 @@ export default function App() {
     <>
       <Header />
       <main className="container">
-        <Entry />
+        <Entry
+          img={{ src: "src/assets/mount-fuji.png", alt: "mount fuji" }}
+          country="Janap"
+          title="Mount Fuji"
+          googleMapLink="https://g.co/kgs/yrwrm7F"
+          dates="12 Jan, 2023 - 24 Jan, 2023"
+          text="Mount Fuji is the tallest mountain in Japan, 
+          standing at 3,776 meters (12,380 feet). Mount Fuji is 
+          the single most popular tourist site in Japan, for both 
+          Japanese and foreign tourists."
+        />
+        <Entry
+          img={{
+            src: "src/assets/sydney-opera-house.png",
+            alt: "sydney opera house",
+          }}
+          country="Australia"
+          title="Sydney Opera House"
+          oogleMapLink=""
+          dates="27 May, 2023 - 8 Jun, 2023"
+          text="The Sydney Opera House is a multi-venue 
+          performing arts centre in Sydney. Located on the banks 
+          of the Sydney Harbour, it is often regarded as one of 
+          the 20th century's most famous and distinctive buildings."
+        />
+        <Entry
+          img={{
+            src: "src/assets/geirangerfjord.png",
+            alt: "sydney opera house",
+          }}
+          country="Norway"
+          title="Geirangerfjord"
+          oogleMapLink=""
+          dates="01 Oct, 2024 - 18 Nov, 2024"
+          text="The Geiranger Fjord is a fjord in the Sunnmøre 
+          region of Møre og Romsdal county, Norway. It is located 
+          entirely in the Stranda Municipality."
+        />
       </main>
     </>
   );
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,26 +1,22 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="journal-entry">
-      <img
-        className="main-image"
-        src="src/assets/mount-fuji.png"
-        alt="mount fuji"
-      />
+      <img 
+        className="main-image" 
+        src={props.img.src} 
+        alt={props.img.alt} 
+        />
       <div className="info-container">
         <img
           className="marker"
           src="src/assets/marker.png"
           alt="map marker icon"
         />
-        <span className="country">Japan</span>
-        <a href="https://g.co/kgs/yrwrm7F">View on Google Maps</a>
-        <h2 className="entry-title">Mount Fuji</h2>
-        <p className="trip-dates">12 Jan, 2023 - 24 Jan, 2023</p>
-        <p className="entry-dates">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapLink}>View on Google Maps</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-dates">{props.text}</p>
       </div>
     </article>
   );
