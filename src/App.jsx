ada19b36ab47fc6b627bcdf7df5fea148686c2c0
Olesia Shcherbenko <oshch96@gmail.com>
2025-01-11 20:41:28 +0100

    feature/map-entry-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data";
 
 export default function App() {
+  const entryElements = data.map((entry) => {
+    return (
+      // eslint-disable-next-line react/jsx-key
+      <Entry
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    );
+  });
+
   return (
     <>
       <Header />
-      <main className="container">
-        <Entry
-          img={{ src: "src/assets/mount-fuji.png", alt: "mount fuji" }}
-          country="Janap"
-          title="Mount Fuji"
-          googleMapLink="https://g.co/kgs/yrwrm7F"
-          dates="12 Jan, 2023 - 24 Jan, 2023"
-          text="Mount Fuji is the tallest mountain in Japan, 
-          standing at 3,776 meters (12,380 feet). Mount Fuji is 
-          the single most popular tourist site in Japan, for both 
-          Japanese and foreign tourists."
-        />
-        <Entry
-          img={{
-            src: "src/assets/sydney-opera-house.png",
-            alt: "sydney opera house",
-          }}
-          country="Australia"
-          title="Sydney Opera House"
-          oogleMapLink=""
-          dates="27 May, 2023 - 8 Jun, 2023"
-          text="The Sydney Opera House is a multi-venue 
-          performing arts centre in Sydney. Located on the banks 
-          of the Sydney Harbour, it is often regarded as one of 
-          the 20th century's most famous and distinctive buildings."
-        />
-        <Entry
-          img={{
-            src: "src/assets/geirangerfjord.png",
-            alt: "sydney opera house",
-          }}
-          country="Norway"
-          title="Geirangerfjord"
-          oogleMapLink=""
-          dates="01 Oct, 2024 - 18 Nov, 2024"
-          text="The Geiranger Fjord is a fjord in the Sunnmøre 
-          region of Møre og Romsdal county, Norway. It is located 
-          entirely in the Stranda Municipality."
-        />
-      </main>
+      <main className="container">{entryElements}</main>
     </>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 export default function Entry(props) {
   return (
     <article className="journal-entry">
