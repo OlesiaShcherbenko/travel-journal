864cf5ab99814a91292b4241f12ee6cbcbdeb46e
Olesia Shcherbenko <oshch96@gmail.com>
2025-01-13 11:18:20 +0100

    feature/spread-object-as-props

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,10 @@ import data from "./data";
 
 export default function App() {
   const entryElements = data.map((entry) => {
-    return (
-      <Entry
-        key={entry.id}
-        img={entry.img}
-        title={entry.title}
-        country={entry.country}
-        googleMapLink={entry.googleMapsLink}
-        dates={entry.dates}
-        text={entry.text}
-      />
-    );
+    return <Entry 
+    key={entry.id} 
+    {...entry}
+    />;
   });
 
   return (
