6051dca6c0d1fa59a2411e4945b1b42784192753
Olesia Shcherbenko <oshch96@gmail.com>
2025-05-19 07:54:36 +0200

    feat: add reusable header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <img src="/src/assets/globe.png" alt="Logo" />
+        <h1>my travel journal.</h1>
+      </header>
+    </>
+  );
+}
