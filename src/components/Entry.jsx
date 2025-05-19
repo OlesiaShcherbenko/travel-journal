b1aab9419c2f42cc2c0d4531995c94886345850d
Olesia Shcherbenko <oshch96@gmail.com>
2025-05-19 16:53:19 +0200

    feat: add reusable Entry component with props support

---
+++ src/components/Entry.jsx
@@ -0,0 +1,19 @@
+export default function Entry(props) {
+  return (
+    <article className="journal-entry">
+      <img src={props.img.src} alt={props.img.alt} />
+      <div className="info-container">
+        <img
+          className="marker"
+          src="/src/assets/marker.png"
+          alt="map marker icon"
+        />
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapsLink}>View on Google Maps</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
+      </div>
+    </article>
+  );
+}
